0cd8c1f738273601c68815397366d8cd8000a804
PINHSIENLIN <billy2003live@gmail.com>
2023-04-07 21:52:06 -0700

    Commit From RStudio

---
+++ app.R
@@ -0,0 +1,19 @@
+library(shiny)
+
+ui <- fluidPage(
+  sliderInput("bins", "Number of bins:", 
+              min = 1, max = 50, value = 30),
+  plotOutput("distPlot")
+)
+
+server <- function(input, output, session) {
+  output$distPlot <- renderPlot({
+    x <- faithful[, 2]  # Old Faithful Geyser data
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    hist(x, breaks = bins, col = 'darkgray', border = 'white',
+         xlab = 'Waiting time to next eruption (in mins)',
+         main = 'Histogram of waiting times')
+  })
+}
+
+shinyApp(ui, server)
